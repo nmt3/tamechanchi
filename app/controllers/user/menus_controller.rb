a1eea4c398bae06b2cb158f779c799844369fb71
nmt3 <nmttkm3@gmail.com>
2023-05-25 09:07:07 +0000

    user_menus

--- app/controllers/user/menus_controller.rb
+++ app/controllers/user/menus_controller.rb
@@ -1,6 +1,11 @@
 class User::MenusController < ApplicationController
   def index
     @menus = Menu.all
+    @genres = Genre.all
+    if params[:genre_id].present?
+      @genre = Genre.find(params[:genre_id])
+      @menus = @genre.menus
+    end
   end
 
   def show
